93d411dee8e227da0412937d6b71d98d877c7a82
Daniel Correa <65257663+danielcorrea256@users.noreply.github.com>
2024-07-23 10:14:30 -0500

    Create FenwickTree.cpp

---
+++ DataStructures/FenwickTree.cpp
@@ -0,0 +1,33 @@
+/*
+Puede añadir dimensiones anidando fors para la suma y resta y aumentando las dimensiones de la variable "bit"
+*/
+struct FenwickTree {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+};
